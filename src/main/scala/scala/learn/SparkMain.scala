bdb2b3e622cd70f0b7178c488b9131c25a06529e
himanshu <himanshusingcs0036@gmail.com>
2019-04-27 15:46:38 +0530

    adding spark session with to main...

--- src/main/scala/scala/learn/SparkMain.scala
+++ src/main/scala/scala/learn/SparkMain.scala
@@ -6,12 +6,36 @@ object SparkMain {
 
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession
-      .builder().config("hive.metastore.uris","thrift://127.0.1.1:9083")
-      .appName("Test App")
+    var spark : SparkSession= null
+
+     if(args(0)=="1"){
+      spark=SparkSession
+      .builder()
       .enableHiveSupport()
-      .master("local[*]")
-      .getOrCreate()
+      .appName("Test App")
+      .master("spark://hdpmaster:7077")
+      .getOrCreate()}else if (args(0)=="2"){
+       spark=SparkSession
+         .builder()
+         .config("spark.sql.warehouse.dir","hdfs://10.0.2.15:9000/user/hive/warehouse")
+         .enableHiveSupport()
+         .appName("Test App")
+         .master("spark://hdpmaster:7077")
+         .getOrCreate()
+
+     } else {
+
+       spark=SparkSession
+         .builder().config("hive.metastore.uris","thrift://localhost:9083")
+         .config("spark.sql.warehouse.dir","hdfs://10.0.2.15:9000/user/hive/warehouse/")
+         .appName("Test App")
+         .master("spark://hdpmaster:7077")
+         .getOrCreate()
+
+     }
+
+    spark.sql("show databases").show()
+    spark.close()
 
   }
 
